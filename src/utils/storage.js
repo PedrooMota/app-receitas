76a9b9fab41e128cb6254b1cd4714ac6ba066109
PedrooMota <pedrohmota2005@gmail.com>
2023-04-09 22:57:09 -0300

    quase finalizado

---
+++ src/utils/storage.js
@@ -0,0 +1,44 @@
+
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+//Buscar os favoritos
+//salvar novo favorito
+//remover favorito da lista
+
+export async function getFavorites(key){
+    const favorites = await AsyncStorage.getItem(key)
+    return JSON.parse(favorites) || [];
+}
+
+export async function saveFavorites(key, newItem){
+    let myFavorites = await getFavorites(key);
+
+    let hasItem = myFavorites.some( item => item.id === newItem.id)
+
+    if(hasItem){
+
+    }
+
+    myFavorites.push(newItem)
+
+    await AsyncStorage.setItem(key, JSON.stringify(myFavorites))
+}
+
+export async function removeItem(id){
+
+    let receipes = await getFavorites("@appreceitas")
+    let myFavorites = receipes.filter( item => {
+        return (item.id !== id)
+    })
+
+    await AsyncStorage.setItem("@appreceitas", JSON.stringify(myFavorites));
+    return myFavorites;
+}
+
+export async function isFavorite(receipe){
+
+    let myReceipes = await getFavorites("@appreceitas")
+
+    const favorite = myReceipes.find( item => item.id === receipe.id)
+}
